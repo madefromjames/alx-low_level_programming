66fc0aa9166034a2dda6e9ac9539ae767ec15495
madefromjames <madefromjames@gmail.com>
2022-10-15 16:10:58 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,19 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
-	       putchar(i);
+		putchar(i);
 
-	       if (i < '9')
-	       {
-		       putchar(',');
-		       putchar(' ');
-	       }
+		if (i < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
