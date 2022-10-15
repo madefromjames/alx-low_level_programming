16092a71b3d1ac28a8aa54115efdc155806f26de
madefromjames <madefromjames@gmail.com>
2022-10-15 05:48:17 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,4 +7,16 @@
  */
 int main(void)
 {
+	char lower, upper;
+
+	for (lower = 97; lower <= 122; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 97; upper <= 122; upper++)
+	{
+		putchar(upper);
+	}
+
+	return (0);
 }
