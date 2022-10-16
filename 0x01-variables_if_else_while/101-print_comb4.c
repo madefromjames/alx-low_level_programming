1d8cb34a8c53ff875817584e0efb3f6b9aba042e
madefromjames <madefromjames@gmail.com>
2022-10-15 18:21:16 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Reduce: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 5; x++)
+	{
+		for (y = x + 1; y <= 6; y++)
+		{
+			for (z = y + 1; z <= 7; z++)
+			{
+				if (x != y && x != y && y != z)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+				}
+				if (x != 5 || y != 6 || z != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
