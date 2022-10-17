d83d9836ee6874874fdf9db5413c804d0bc3998b
madefromjames <madefromjames@gmail.com>
2022-10-17 16:37:10 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = a; c < 9; c++)
+			{
+				for (d = c + 1; d < 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (a != 9 || b != 9 || c != 9 || d != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
