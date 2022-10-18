3aaa9d4b84742b85cdffc67e40c917d3f48affe5
madefromjames <madefromjames@gmail.com>
2022-10-18 16:02:32 -0700

    hard

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar, followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(1, "_putchar", strlen("_putchar"));
+	putchar(10);
+	return 0;
+}
