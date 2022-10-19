2828b7caa94864124cda6ed3028731a5f451d57f
madefromjames <madefromjames@gmail.com>
2022-10-18 17:35:07 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,10 @@ int _putchar(char c);
 
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
+
+void print_alphabet_x10(void);
+
 #endif /* _MAIN_H_ */
