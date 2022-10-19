0d5992eae28be66dea8d14e96bdc92b0988c77eb
madefromjames <madefromjames@gmail.com>
2022-10-19 06:35:23 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Prototype
+ * @c: parameter
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,12 @@ void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
+/**
+ * _islower - checks for lowercase
+ * @c: the character to check
+ * Return: int
+ */
+
+int _islower(int c);
+
 #endif /* _MAIN_H_ */
