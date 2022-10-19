e46420d84ef19e59a55c2c69167297ff027e0a41
madefromjames <madefromjames@gmail.com>
2022-10-19 12:00:56 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer.
+ * @: absolute integer
+ *
+ * Return: Always 0
+ */
+int _abs(int r)
+{
+	if (r < 0)
+		_putchar(r);
+	else
+		_putchar(r);
+	return (0);
+}
