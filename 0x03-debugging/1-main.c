d2827048cb01b9d3ff7c5513a9457d062fd4fa5b
madefromjames <madefromjames@gmail.com>
2022-10-20 08:43:03 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :\(");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	printf("Infinte loop avoided! \\o/");
+
+	putchar(10);
+	return (0);
+}
