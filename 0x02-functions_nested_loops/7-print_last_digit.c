dff8c13fdf7b66801f5df52a40f03d23a8647a0b
madefromjames <madefromjames@gmail.com>
2022-10-19 18:08:11 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @v: check the digit
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int v)
+{
+	if (v % 25)
+		_putchar(v);
+	return (0);
+}
