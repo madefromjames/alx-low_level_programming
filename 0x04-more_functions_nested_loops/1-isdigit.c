4e33594658ba6d48ad37145db3a6966bb4294921
madefromjames <madefromjames@gmail.com>
2022-10-22 05:40:09 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - checks for a digit (0 through 9)
+ * @c: returns digit
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 47 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
