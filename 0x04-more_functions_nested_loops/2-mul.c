b0e310db17f5c7c0e3de1eb1c5ad319cc3751056
madefromjames <madefromjames@gmail.com>
2022-10-22 06:16:06 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mull - multiply two integers
+ * @a: return a
+ * @b: return b
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
