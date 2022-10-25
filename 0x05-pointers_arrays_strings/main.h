f086c98a970db4c30c18d10ac5cf4e8bc41fd09a
madefromjames <madefromjames@gmail.com>
2022-10-24 18:44:41 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,6 +12,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 
 
