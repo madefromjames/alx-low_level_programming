bfc625d17698bbe767791f925ac8b2ed14eb2d25
madefromjames <madefromjames@gmail.com>
2022-10-24 18:21:16 -0700

    4-main.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 
 
 
