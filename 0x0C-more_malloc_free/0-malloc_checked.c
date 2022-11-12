85c38072739eab86438bb65b41c26742d628f08d
madefromjames <madefromjames@gmail.com>
2022-11-11 17:21:16 -0800

    malloc more

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: unsigned integer
+ * Return: Always 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *allocateMemory = malloc(b);
+
+	if (allocateMemory == NULL)
+		exit(98);
+
+	return (allocateMemory);
+}
