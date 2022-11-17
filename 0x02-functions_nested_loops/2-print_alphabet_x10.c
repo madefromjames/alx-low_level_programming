4d1fc061180138186609f51897066ceed2feba62
madefromjames <madefromjames@gmail.com>
2022-11-17 08:55:32 -0800

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - 10 times alphabet in lowercase
- * 
+ *
  * Return: 10 times alphabet
  */
 
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		for (j = 97; j < 123; j++)
 			_putchar(j);
