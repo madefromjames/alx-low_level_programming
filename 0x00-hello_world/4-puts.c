5f751d66e342aca78e1f8651dd876473de35bcc7
madefromjames <madefromjames@gmail.com>
2023-06-15 13:13:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints a string
+ *
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
