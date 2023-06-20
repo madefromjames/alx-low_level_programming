d358eb78e546a3fde688d73b3007b3de60c3a59d
madefromjames <madefromjames@gmail.com>
2023-06-20 00:45:59 -0700

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print two numbers
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for(j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			{
+				if (i != 5 || (i == 5 && j != 6))
+				{
+					putchar(',');
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
