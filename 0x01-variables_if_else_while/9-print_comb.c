fc4af898663c83897ed3ea2aafccd91891e11afb
madefromjames <madefromjames@gmail.com>
2023-06-20 00:08:11 -0700

    single number combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print singe-digit numbers
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i <= 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
