cafaf5e2619ff650f237ad98e4a81f9e039196c7
madefromjames <madefromjames@gmail.com>
2023-06-21 06:08:43 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check alpha characters
+ * @c: the char to be checked
+ *
+ * Return: 0;
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
