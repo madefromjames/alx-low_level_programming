d8fd48072250884c006500e56e3830c63d07e326
madefromjames <madefromjames@gmail.com>
2023-06-21 09:29:57 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @i: return last digit
+ *
+ * Return: 0;
+ */
+int print_last_digit(int i)
+{
+	if (i % 2)
+		return (i);
+
+	return (0);
+}
