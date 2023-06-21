b967c9d6404540eebf39e6c7636174cb4b8a0e6e
madefromjames <madefromjames@gmail.com>
2023-06-21 01:44:18 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10(void) - alphabet x10
+ *
+ * Return: 0;
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar(10);
+	}
+}
