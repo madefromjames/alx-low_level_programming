8f873e4f78f2de7681fc90e62e966ae67b591934
madefromjames <madefromjames@gmail.com>
2023-06-21 05:05:34 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - check for lowercase
+ *
+ * Return: 0;
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		_putchar(c);
+	return (10);
+}
