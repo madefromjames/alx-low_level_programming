2468458a834ddd3a497dda8c448b88b0a953a560
madefromjames <madefromjames@gmail.com>
2023-06-21 09:16:31 -0700

    absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - absolute integer
+ * @i: returns integer
+ *
+ * Return: 0;
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (i);
+	else if (i > 0)
+		return (i);
+
+	return (0);
+}
