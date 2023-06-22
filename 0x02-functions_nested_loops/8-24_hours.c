9975b267ed7e72c2edc70232b7f43f9155d6572c
madefromjames <madefromjames@gmail.com>
2023-06-22 03:40:28 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: 0;
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a < '3'; a++)
+	{
+		_putchar('0' + a);
+		for (b += a; b <= '9'; b++)
+		{
+			_putchar('0' + b);
+			_putchar(58);
+			for (c = '0'; c < '6'; c++)
+			{
+				_putchar('0' + c);
+				for (d += c; d <= '9'; d++)
+				{
+					_putchar('0' + d);
+					_putchar(10);
+				}
+			}
+		}
+	}
+}
