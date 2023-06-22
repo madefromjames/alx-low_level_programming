06d785e97a838873fc74407b9a35683aff9d3e20
madefromjames <madefromjames@gmail.com>
2023-06-22 15:17:21 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integer
+ * @num: first number
+ * @num: second number
+ *
+ * Return: 0;
+ */
+int add(int num1, int num2)
+{
+	int twoDigit = num1 + num2;
+	return twoDigit;
+
+	return (0);
+}
