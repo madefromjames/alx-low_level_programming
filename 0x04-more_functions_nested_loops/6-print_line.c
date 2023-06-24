879811589253c4f47fac1d0caaca9799c0150e62
madefromjames <madefromjames@gmail.com>
2023-06-24 14:47:11 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ * @n: n should print _
+ */
+void print_line(int n)
+{
+	int line;
+
+	for (line = 1; line <= n; line++)
+	{
+		if (line < n)
+			_putchar(95);
+		else if (line < 0)
+			_putchar(10);
+	}
+	_putchar(10);
+}
