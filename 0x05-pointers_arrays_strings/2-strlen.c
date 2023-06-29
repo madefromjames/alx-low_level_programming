144765871995d9a95f7868ea49e54365fe95893b
madefromjames <madefromjames@gmail.com>
2023-06-29 11:18:18 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - length of string
+ * @s: string parameter
+ * Return: length
+ */
+int  _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	return (len);
+}
