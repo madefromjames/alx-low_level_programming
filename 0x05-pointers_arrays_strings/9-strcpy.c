b1d1cc9b8a151f4623e04bdf8d6c1ceec9138a6c
madefromjames <madefromjames@gmail.com>
2023-06-30 08:29:52 -0700

    copy n paste strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copy string from src to dest
+ * @dest: destination
+ * @src: source
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	for (len = 0; src[len] != 0; len++)
+	{
+		dest[len] = src[len];
+	}
+	return (dest);
+}
