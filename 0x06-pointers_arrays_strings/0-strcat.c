f219730c16ee1b1dbf58fc40d895e9327c629dcc
madefromjames <madefromjames@gmail.com>
2023-07-03 02:40:16 -0700

    string concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strcat - joins two string
+ * @dest: string destination to be copied
+ * @src: string source to be copied
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2;
+
+	for (len1 = 0; dest[len1] != '\0'; len1++)
+	{
+	}
+
+	for (len2 = 0; src[len2] != '\0'; len2++)
+	{
+		dest[len1] = src[len2];
+		len1++;
+	}
+	return (dest);
+}
